36e9bfd975a4e9e77e7c51fefe197436fbcd70d3
TheProgrammerofalltime <kma69362@gmail.com>
2025-10-01 19:54:41 -0700

    commit

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const path = require('path')
 const app = express()
 POST = 3000
 
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'public', 'HTML', 'introduction.html'));
 });
+
+app.get('/puzzle-1', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'HTML', 'puzzle1.html'))
+})
+
+app.get('/puzzling', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'HTML', 'puzzle2.html'))
+})
+
+app.get('/final', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'public', 'HTML', 'welcome.html'))
+})
 
 app.listen(POST, () =>{
     console.log(`Server is running on http://localhost:${POST}`);
